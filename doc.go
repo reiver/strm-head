eba5692f91aaeaf8c1b42be73878650eeea0c359
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-10-18 23:25:18 -0700

    updated docs a bit.

--- doc.go
+++ doc.go
@@ -1,7 +1,9 @@
 /*
-A driver for the **go-strm** library, that provides the **HEAD** Beginner command.
+A driver for the go-strm library, that provides the HEAD Beginner command.
 
 YOU SHOULD ONLY IMPORT THIS PACKAGE USING EITHER A DOT IMPORT OR AN UNDERSCORE IMPORT.
+
+(The package is named "verboten" on purpose to discourage people from importing by name.)
 
 For example:
 
